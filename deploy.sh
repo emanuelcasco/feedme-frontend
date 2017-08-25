6a06e86c78bd6bb9453252c165360464dc9a0008
Emanuel <cascoemanuel@gmail.com>
2017-08-25 20:09:21 -0300

    Adding dist folder

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-app_name=$(heroku apps:info | grep === | awk '{print $2}')
-version=$(git rev-parse HEAD)
-yarn build
-$(cd dist && heroku builds:create --version "${version}" -a ${app_name} --tar $(which gtar))
