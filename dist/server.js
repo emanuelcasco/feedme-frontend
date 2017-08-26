7f0b9a102e992e255e2db6e19ec7527c01479d50
Emanuel <cascoemanuel@gmail.com>
2017-08-25 23:32:41 -0300

    Routing

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,20 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-var history = require('connect-history-api-fallback')
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
 
-var app = express()
-app.use(serveStatic(__dirname))
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
+
+app.use(staticFileMiddleware)
 app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
-var port = process.env.PORT || 5000
-app.listen(port)
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname, '/index.html'))
+})
 
-console.log('server started ' + port)
+app.listen(5000, function () {
+  console.log('Express serving on 5000!')
+})
